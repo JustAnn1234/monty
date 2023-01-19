1a6d5379388c5588da153f3cfa82f69ece80ba28
JustAnn1234 <hassanomosomi@gmail.com>
2023-01-19 20:04:24 +0100

    updated

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-This is my monty
+My monty task
--- monty_main.c
+++ monty_main.c
@@ -1,9 +1,15 @@
 #include "monty.h"
+#include <stdio.h>
+#define _GNU_SOURCE
+#include <stdlib.h>
+
 bus_t bus = {NULL, NULL, NULL, 0};
+
 /**
-* main - monty code interpreter
-* @argc: number of arguments
-* @argv: monty file location
+* main - function for monty code interpreter
+* @argc: argument count
+* @argv: argument value
+*
 * Return: 0 on success
 */
 int main(int argc, char *argv[])
@@ -41,5 +47,5 @@ int main(int argc, char *argv[])
 	}
 	free_stack(stack);
 	fclose(file);
-return (0);
+	return (0);
 }
